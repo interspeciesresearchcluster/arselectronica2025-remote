c8f1ef29b74e97aabba9aa0a9e0f5ccaad9017a8
Max Baraitser Smith <m.baraitsersmith@gmail.com>
2025-08-29 13:44:28 +0200

    fhajjkh

--- control_signal_client/control_signal_client.sh
+++ control_signal_client/control_signal_client.sh
@@ -4,6 +4,6 @@
 
 while :
 do
-    /home/maxbaraitsersmith/Desktop/arselectronica2025-remote/control_signal_client/venv/bin/python /home/maxbaraitsersmith/Desktop/arselectronica2025-remote/control_signal_client/control_signal_client.py
+    /home/maxbaraitsersmith/Desktop/arselectronica2025-remote/control_signal_client/venv/bin/python /home/maxbaraitsersmith/Desktop/arselectronica2025-remote/control_signal_client/control_signal_client.py >> /home/maxbaraitsersmith/Desktop/arselectronica2025-remote/control_signal_client/control_signal_client.log
     sleep 5
 done
--- streaming_client/streaming_client_2.sh
+++ streaming_client/streaming_client_2.sh
@@ -2,6 +2,6 @@
 
 while :
 do
-    nc multispeciesresearchcluster.ddnsfree.com 19063 | mpv --no-cache --untimed --no-demuxer-thread  --fs -
+    nc multispeciesresearchcluster.ddnsfree.com 19063 | mpv --no-cache --untimed --no-demuxer-thread  --fs - >> /home/maxbaraitsersmith/Desktop/arselectronica2025-remote/streaming_client/streaming_client_2.log
     sleep 5
 done
