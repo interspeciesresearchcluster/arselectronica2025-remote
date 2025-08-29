efbf84881e7279ac3942f7048860a7b31bc21135
Max Baraitser Smith <m.baraitsersmith@gmail.com>
2025-08-29 14:06:41 +0200

    afwafwafwaw

--- control_signal_client/control_signal_client.sh
+++ control_signal_client/control_signal_client.sh
@@ -4,6 +4,8 @@
 
 while :
 do
+    echo "Command exited, waiting 5 seconds..." >> /home/maxbaraitsersmith/Desktop/arselectronica2025-remote/control_signal_client/control_signal_client.log
     /home/maxbaraitsersmith/Desktop/arselectronica2025-remote/control_signal_client/venv/bin/python /home/maxbaraitsersmith/Desktop/arselectronica2025-remote/control_signal_client/control_signal_client.py >> /home/maxbaraitsersmith/Desktop/arselectronica2025-remote/control_signal_client/control_signal_client.log
+    echo "Command exited, waiting 5 seconds..." >> /home/maxbaraitsersmith/Desktop/arselectronica2025-remote/control_signal_client/control_signal_client.log
     sleep 5
 done
--- streaming_client/streaming_client_1.sh
+++ streaming_client/streaming_client_1.sh
@@ -2,6 +2,8 @@
 
 while :
 do
+    echo "Starting command..." >> /home/maxbaraitsersmith/Desktop/arselectronica2025-remote/streaming_client/streaming_client_1.log
     nc multispeciesresearchcluster.ddnsfree.com 19062 | mpv --no-cache --untimed --no-demuxer-thread  --fs - >> /home/maxbaraitsersmith/Desktop/arselectronica2025-remote/streaming_client/streaming_client_1.log
+    echo "Command exited, waiting 5 seconds..." >> /home/maxbaraitsersmith/Desktop/arselectronica2025-remote/streaming_client/streaming_client_1.log
     sleep 5
 done
