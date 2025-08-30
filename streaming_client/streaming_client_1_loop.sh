16b34ca5fecb5a0a328a860dc4832779995e2b5d
Max Baraitser Smith <m.baraitsersmith@gmail.com>
2025-08-30 12:28:09 +0200

    ffwafafa

--- streaming_client/streaming_client_1_loop.sh
+++ streaming_client/streaming_client_1_loop.sh
@@ -3,7 +3,7 @@
 while :
 do
     echo "Starting command..."
-    nc multispeciesresearchcluster.ddnsfree.com 19062 | mpv --no-cache --untimed --no-demuxer-thread  --fs -
+    bash /home/maxbaraitsersmith/Desktop/arselectronica2025-remote/streaming_client/streaming_client_1.bash"
     echo "Command exited, waiting 5 seconds..."
     sleep 5
 done
---
+++ streaming_client/streaming_client_2_loop.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+while :
+do
+    echo "Starting command..."
+    bash /home/maxbaraitsersmith/Desktop/arselectronica2025-remote/streaming_client/streaming_client_2.bash"
+    echo "Command exited, waiting 5 seconds..."
+    sleep 5
+done
